fab729d90078669cf2422997404c93abf2b8f912
Thomas <thomas.flantua@gmail.com>
2022-02-16 11:11:39 +0100

    Auto stash before rebase of "origin/master"

---
+++ XC8Application1/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/8/2022 12:40:59 PM
+ *  Author: thoma
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
