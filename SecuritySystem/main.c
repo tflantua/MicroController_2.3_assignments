31ab5a99b8ed6bfd460313631444e96f7958aeae
Thomas <t.flantua@student.avans.nl>
2022-03-23 14:15:03 +0100

    Made new project for SecuritySystem.

---
+++ SecuritySystem/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 3/23/2022 2:11:34 PM
+ *  Author: thoma
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
