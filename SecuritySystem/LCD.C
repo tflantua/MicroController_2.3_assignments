43e9e31656cc17ec18c733f453da67e112908048
r <r@a.ml>
2022-04-04 14:06:23 +0200

    Laser working and photo resister reading.

--- SecuritySystem/LCD.C
+++ SecuritySystem/LCD.C
@@ -29,7 +29,6 @@ void init_4bits_mode(void);
 void lcd_write_string(char *str);
 void lcd_write_data(unsigned char byte);
 void lcd_write_cmd(unsigned char byte);
-void lcd_clear(void);
 
 
 /******************************************************************
--- SecuritySystem/main.c
+++ SecuritySystem/main.c
@@ -1,9 +1,9 @@
 /*
- * main.c
- *
- * Created: 3/23/2022 2:11:34 PM
- *  Author: thoma
- */ 
+* main.c
+*
+* Created: 3/23/2022 2:11:34 PM
+*  Author: thoma
+*/
 
 #include <xc.h>
 #include "waiting.h"
@@ -16,19 +16,44 @@ void adcInit(void){
 
 int main(void)
 {
+	DDRC = 0xFF;			// PORTD(7) output, PORTD(6:0) input
+	PORTC = 0xFF;
 	init();
 	
+	DDRF = 0x00;
 	DDRA = 0xFF;
 	DDRB = 0xFF;
-	DDRD = 0xFF;
-	DDRF = 0x00;
-	
-    PORTD = 0x01;
 	adcInit();
 	
+	char text_print[20];
+	unsigned long int waarde;
+	long l = 0;
+	long h  = 0;
+	
+	DDRD = 0x00;
 	while(1){
-		int value = ADCH << 8 | ADCL;
-		display_text((char *) value);
+		l = ADCL;
+		h  = ADCH;
+		PORTA = l;
+		PORTB = h;
+		
+		waarde = (h << 8) | l;
+		
+		sprintf(text_print, "waarde %lu", waarde);
+		
+		display_text(text_print);
 		waitFunction(100);
+		
+		lcd_clear();
 	}
+	
+	//
+	//PORTD = 0x01;
+
+	//
+	//while(1){
+	//int value = ADCH << 8 | ADCL;
+	//display_text((char *) value);
+	//waitFunction(100);
+	//}
 }
